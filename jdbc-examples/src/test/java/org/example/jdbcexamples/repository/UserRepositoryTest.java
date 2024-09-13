8c2c163a71592f11b350db9c5d486284ce929790
Hushyo <Hushsama@hotmail.com>
2024-09-13 09:36:09 +0800

    sad
    asd

---
+++ jdbc-examples/src/test/java/org/example/jdbcexamples/repository/UserRepositoryTest.java
@@ -0,0 +1,11 @@
+package org.example.jdbcexamples.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Slf4j
+class UserRepositoryTest {
+
+}
